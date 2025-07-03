8e5b5d93857b22b5a754a23e98fa303b913df56a
Chandan Kumar Singh <chandanelectrovese@gmail.com>
2025-07-04 01:07:00 +0530

    nothing

--- server/routes/apis/admin/web_page.routes.js
+++ server/routes/apis/admin/web_page.routes.js
@@ -1,17 +1,17 @@
 
 import express from "express";
-import PageController from "../../../controllers/admin/pageController.js";
+// import PageController from "../../../controllers/admin/pageController.js";
 import * as WebSchema from "../../../middlewares/validators/page.validators.js";
 import { AdminMiddleware } from "../../../middlewares/authenticate.js";
 import SchemaValidator from "../../../middlewares/SchemaValidator.js";
 
 const router = express.Router();
 
-router.get("/list", AdminMiddleware, PageController.list);
-router.post("/create", AdminMiddleware, SchemaValidator(WebSchema.createWebPage), PageController.create);
-// router.get("/:id", AdminMiddleware, PageController.getAllPages);
-// router.get("/:slug", PageController.getPageBySlug);
-router.post("/update/:id", AdminMiddleware, SchemaValidator(WebSchema.updateWebPage), PageController.updateWebPage);
-router.delete("/delete/:id", AdminMiddleware, SchemaValidator(WebSchema.deleteWebPage), PageController.delete);
+// router.get("/list", AdminMiddleware, PageController.list);
+// router.post("/create", AdminMiddleware, SchemaValidator(WebSchema.createWebPage), PageController.create);
+// // router.get("/:id", AdminMiddleware, PageController.getAllPages);
+// // router.get("/:slug", PageController.getPageBySlug);
+// router.post("/update/:id", AdminMiddleware, SchemaValidator(WebSchema.updateWebPage), PageController.updateWebPage);
+// router.delete("/delete/:id", AdminMiddleware, SchemaValidator(WebSchema.deleteWebPage), PageController.delete);
 
 export default router;
