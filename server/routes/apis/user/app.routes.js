fede2493aeaf32d73a59fab0f8697cee46c8fe20
Chandan Kumar Singh <chandanelectrovese@gmail.com>
2025-07-04 01:08:57 +0530

    nothing

--- server/routes/apis/user/app.routes.js
+++ server/routes/apis/user/app.routes.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import { assignQueryAndParamsToBody } from '../../../middlewares/index.js';
 import AppSettingsController from '../../../controllers/app/AppSettingsController.js';
 import { UserMiddleware } from '../../../middlewares/authenticate.js';
-import PageController from '../../../controllers/admin/pageController.js';
+// import PageController from '../../../controllers/admin/pageController.js';
 const router = Router();
 
 router.get('/settings', assignQueryAndParamsToBody, UserMiddleware, AppSettingsController.detail);
-router.get('/pages', assignQueryAndParamsToBody, UserMiddleware, PageController.list);
+// router.get('/pages', assignQueryAndParamsToBody, UserMiddleware, PageController.list);
 
 export default router;
