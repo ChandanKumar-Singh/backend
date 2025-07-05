e7bc72345c25282b5702558134bcc8fcd29bfbf5
Chandan Kumar Singh <chandanelectrovese@gmail.com>
2025-07-05 10:15:46 +0530

    firebase credentials as bse 64

--- server/core/firebase.js
+++ server/core/firebase.js
@@ -2,16 +2,18 @@ import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
 import { dirname } from "../utils/PathUtils.js";
-import {logg, LogUtils} from "../utils/logger.js"
+import { logg, LogUtils } from "../utils/logger.js"
 
 try {
-    const filePath = path.resolve(dirname, "../../rental-room-management-system-firebase-adminsdk-fbsvc-99617f3842.json");
-    const serviceAccount = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    // const filePath = path.resolve(dirname, "../../rental-room-management-system-firebase-adminsdk-fbsvc-99617f3842.json");
+    // const serviceAccount = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const serviceAccountBuffer = Buffer.from(process.env.FIREBASE_SECRET_KEY, "base64");
+    const serviceAccount = JSON.parse(serviceAccountBuffer.toString("utf-8"));
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     });
     LogUtils.info("✅ Firebase Admin SDK initialized successfully.");
 } catch (error) {
     LogUtils.error("❌ Firebase Admin SDK initialization failed:", error);
-} 
+}
 
